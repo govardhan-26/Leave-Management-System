72f29224314739250355ff4dfea58755bef5df89
SINGA SAIPRASAD <singasaiprasad21@gmail.com>
2024-03-22 12:05:48 +0530

    fjfjh

--- server/controllers/Employee/EmployeeControllers.js
+++ server/controllers/Employee/EmployeeControllers.js
@@ -1,6 +1,7 @@
 const EmployeeCreateService = require("../../services/Employee/EmployeeCreateService");
 const Employee = require("../../models/Employee");
 const EmployeeListService = require("../../services/Employee/EmployeeListService");
+
 /**
  * @desc Employee Create
  * @access private
@@ -24,36 +25,55 @@ const EmployeeCreate = async (req,res,next)=>{
  * @method GET
  */
 
+
+
 const EmployeeList = async (req, res, next) => {
-  const searchKeyword = req.params.DepartmentId;
-  console.log(searchKeyword);
-
-  let query = {};
-
-  // Check if searchKeyword is a valid phone number
-  if (/^\d{10}$/.test(searchKeyword)) {
-      // If it's a valid phone number, search directly on Phone field
-      query.Phone = searchKeyword;
-  } else {
-      // If it's not a phone number, perform case-insensitive partial match on other fields
-      let searchRgx = new RegExp(searchKeyword, 'i');
-      query.$or = [
-          { FirstName: searchRgx },
-          { LastName: searchRgx },
-          { Gender: searchRgx },
-          { Address: searchRgx },
-          { Email: searchRgx }
-      ];
-  }
-
+//   const deptId  = req.params;
+const DepartmentId = req.params.DepartmentId;
+console.log(DepartmentId);
   try {
-      const result = await Employee.find(query);
-      console.log(result);
-      res.json(result);
+    const result = await EmployeeListService(req, Employee,  DepartmentId);
+    res.json(result);
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
+
+
+
+
+// const EmployeeList = async (req, res, next) => {
+//   const searchKeyword = req.params.DepartmentId;
+//   console.log(searchKeyword);
+
+//   let query = {};
+
+//   // Check if searchKeyword is a valid phone number
+//   if (/^\d{10}$/.test(searchKeyword)) {
+//       // If it's a valid phone number, search directly on Phone field
+//       query.Phone = searchKeyword;
+//   } else {
+//       // If it's not a phone number, perform case-insensitive partial match on other fields
+//       let searchRgx = new RegExp(searchKeyword, 'i');
+//       query.$or = [
+//           { FirstName: searchRgx },
+//           { LastName: searchRgx },
+//           { Gender: searchRgx },
+//           { Address: searchRgx },
+//           { Email: searchRgx }
+//       ];
+//   }
+
+//   try {
+//       const result = await Employee.find(query);
+//       console.log(result);
+//       res.json(result);
+//   } catch (error) {
+//       next(error);
+//   }
+// };
+
+
 
 
 
